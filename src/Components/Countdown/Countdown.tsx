afc881fc53316533f9b7b8b3458c24aaf9272f6a
amanahmedkhan <amanahmedkhan1@gmail.com>
2023-09-17 18:51:48 +0400

    remove unnecessary files

--- src/Components/Countdown/Countdown.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { ICountdown } from "./Countdown.types";
-
-export const Countdown = (props: ICountdown): JSX.Element => {
-    return (<div>Countdown {props.count}</div>);
-};
--- src/Components/Countdown/Countdown.types.ts
+++
@@ -1,3 +0,0 @@
-export interface ICountdown {
-    count: number;
-}
