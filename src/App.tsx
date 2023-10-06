45ccf91660af1981ee1f8bde8ae48c50bf8be1ea
amanahmedkhan <amanahmedkhan1@gmail.com>
2023-10-07 01:57:31 +0400

    Style Header and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import { Footer } from './Components/Footer/Footer';
 const App = (): JSX.Element => {
   return (
     <div>
-      <Header heading='Main Header' />
+      <Header heading='React Pre Build Components' navItem='Home' />
       <div>Welcome React App</div>
-      <Footer/>
+      <Footer heading="Copyright"/>
     </div>
   )
 }
--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import * as S from "./Footer.styled";
+import { IFooter } from "./Footer.types";
 
-export const Footer = () => {
+export const Footer = ({ heading }: IFooter): JSX.Element => {
     return(
-        <footer>Main Footer</footer>
+        <S.FooterSection>
+            {heading}
+        </S.FooterSection>
     )
 };
