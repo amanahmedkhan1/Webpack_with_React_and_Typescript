698cbbfab05690124250060cfc2c02322ab09f87
amanahmedkhan <amanahmedkhan1@gmail.com>
2023-10-11 01:06:52 +0400

    file changes for increment decrement functionality

--- src/Components/Countdown/Countdown.tsx
+++ src/Components/Countdown/Countdown.tsx
@@ -5,18 +5,30 @@ import * as S from "./Countdown.styled";
 export const Countdown = ({ heading, count }: ICountdown): JSX.Element => {
     const [currentCount, setCurrentCount] = useState<number>(count);
     
-    const increment = () => {
-        setCurrentCount(prevState => {
-            return prevState + 1;
-        });
+    // const increment = () => {
+    //     setCurrentCount(prevState => {
+    //         return prevState + 1;
+    //     });
+    // }
+
+    // const decrement = () => {
+    //     setCurrentCount(prevState => {
+    //         return prevState - 1;
+    //     });
+    // }
+    
+    const incrementDecrement = (increment : boolean) => {
+        if(increment = true){
+            setCurrentCount(prevState => {
+                return prevState + 1;
+            });
+        }else{
+            setCurrentCount(prevState => {
+                return prevState - 1;
+            });
+        };
     }
 
-    const decrement = () => {
-        setCurrentCount(prevState => {
-            return prevState - 1;
-        });
-    }
-    
     return (
         <S.CountdownWrapper>
             <S.CountdownHeading>
@@ -28,10 +40,10 @@ export const Countdown = ({ heading, count }: ICountdown): JSX.Element => {
             </S.CountdownHeading>
             
             <S.ButtonWrapper>
-                <S.CountdownButton onClick={decrement}>
+                <S.CountdownButton onClick={() => incrementDecrement(false)}>
                     - Dicressed
                 </S.CountdownButton>
-                <S.CountdownButton color={'blue'} onClick={increment}>
+                <S.CountdownButton color={'blue'} onClick={() => incrementDecrement(true)}>
                     + Incressed
                 </S.CountdownButton>
             </S.ButtonWrapper>
